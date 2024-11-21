9381c74d2480f8955b41d111dbccc4b0015aee08
DeviPothula <devikapothula597@gmail.com>
2024-11-21 17:49:08 +0530

    Dockerfileadded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple react without nginx as web server to host instead using eks</h1>
+      <h2>Using Kubernetes as container orchestration </h2>
+      <h3>Amazon EKS :</h3>
+      <p>Deployed successfully </p>
+    </div>
+  );
+}
+
+export default App;
