649a8fe471610064358004c5f2fd0cc30b97e9c8
DeviPothula <devikapothula597@gmail.com>
2024-12-04 16:11:09 +0530

    test github hook

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
       <p>Deployed successfully </p>
       <p>New change to test deployment 2 !!!</p>
       <p style={{ color: 'red' }}>New change to test deployment 3 !!! please work</p>
-      <p style={{color: 'green'}}>Rollout command added to restart the server after every build....</p>
+      <p style={{ color: 'green' }}>Rollout command added to restart the server after every build....</p>
+      <p>Test github hook commit</p>
     </div>
   );
 }
