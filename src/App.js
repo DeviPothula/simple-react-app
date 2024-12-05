565cdf18570b0b5923d7b7990027ef5a2cf323fd
DeviPothula <devikapothula597@gmail.com>
2024-12-05 12:14:06 +0530

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <p>Deployed successfully </p>
       <p>New change to test deployment 2 !!!</p>
       <p style={{ color: 'red' }}>New change to test deployment 3 !!! please work</p>
-      <p style={{ color: 'green' }}>Rollout command added to restart the server after every build....</p>
     </div>
   );
 }
